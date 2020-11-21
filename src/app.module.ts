91f4112e46ce81f93b64d830b50fa1d68cc1b4c5
greyewi1 <greyewi@gmail.com>
2020-11-21 14:22:06 +0200

    add get item

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { configService } from './config/config.service'
+import { ItemModule } from './item/item.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
+  imports: [
+    ItemModule,
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
